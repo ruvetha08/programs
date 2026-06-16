da64ab4efa52442711151eaa5183bc150d3d1f37
ruvetha08 <ruvethaabharathi19_bcs27@mepcoeng.ac.in>
2026-06-16 12:17:54 +0530

    Implement search in rotated sorted array

---
+++ Searching.py
@@ -0,0 +1,23 @@
+def search_rotated(arr, key):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == key:
+            return mid
+        if arr[low] <= arr[mid]:
+            if arr[low] <= key < arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:
+            if arr[mid] < key <= arr[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+    return -1
+
+n = int(input("Enter N: "))
+arr = list(map(int, input("Enter array elements: ").split()))
+key = int(input("Enter key: "))
+print(search_rotated(arr, key))
