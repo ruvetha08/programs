f7bfbdafa869ade7b448eb23c3f8d3e290dd9bcd
ruvetha08 <ruvethaabharathi19_bcs27@mepcoeng.ac.in>
2026-06-16 12:02:28 +0530

    Update Frog-2.py

--- Frog-2.py
+++ Frog-2.py
@@ -10,8 +10,6 @@ def solve():
     dp = [float('inf')] * N
     dp[0] = 0
     for i in range(1, N):
-        # Check all valid jumps backward from stone i, up to K steps
-        # min(i, K) ensures we don't look back past the first stone (index 0)
         for j in range(1, min(i, K) + 1):
             cost = dp[i - j] + abs(h[i] - h[i - j])
             if cost < dp[i]:
