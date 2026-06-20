41debf2fabe711f49727238238ad4f24e3c9d35a
ruvetha08 <ruvethaabharathi19_bcs27@mepcoeng.ac.in>
2026-06-20 11:25:37 +0530

    Create ClimbStairs.py

---
+++ ClimbStairs.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n<=2:
+            return n
+        ons=1
+        tws=2
+        for i in range(3,n+1):
+            curr=ons+tws
+            return curr
